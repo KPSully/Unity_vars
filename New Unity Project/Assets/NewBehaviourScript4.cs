9d2d967ca937a14e02344820a6c2db2fa8ae777c
K.P. Sullivan <MacabreSkull@gmail.com>
2017-02-03 10:46:07 -0700

    Update

---
+++ New Unity Project/Assets/NewBehaviourScript4.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript4 : MonoBehaviour {
+
+    public int addition = 12 + 4;
+    public int subtraction = 12 - 4;
+    public float division 5.0f / 2.0f;
+    public int multiplication = 10 * 2;
+    public int remainder = 7 % 2;
+
+	// Use this for initialization
+	void Start () {
+        addition += 9;
+        subtraction -=9:
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
